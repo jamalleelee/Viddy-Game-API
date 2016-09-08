d34349431597f75f9814ac5c68cb643fbbbda133
Jamal <jamal.l.mcdowell@gmail.com>
2016-09-08 09:53:31 -0400

    inital commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# viddygameapi
+
+a [Sails](http://sailsjs.org) application
---
+++ api/controllers/ViddygamesController.js
@@ -0,0 +1,11 @@
+/**
+ * ViddygamesController
+ *
+ * @description :: Server-side logic for managing viddygames
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
